96aa2031531345cf4eb9d3429662d491abc3e420
Michael Lieshoff <michael.lieshoff@virtual-solution.com>
2023-07-11 11:21:58 +0200

    Apply format

--- src/main/java/brawljars/api/intern/rankings/brawler/BrawlerRankingsResponse.java
+++ src/main/java/brawljars/api/intern/rankings/brawler/BrawlerRankingsResponse.java
@@ -18,6 +18,4 @@ package brawljars.api.intern.rankings.brawler;
 
 import brawljars.common.PaginationResponse;
 
-public class BrawlerRankingsResponse extends PaginationResponse<BrawlerRanking> {
-
-}
+public class BrawlerRankingsResponse extends PaginationResponse<BrawlerRanking> {}
